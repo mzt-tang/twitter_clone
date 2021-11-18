0ad0c47c2faa1af1bb14b6963c8689ef37e5b3ac
Michael Tang <michael.tang@optimalworkshop.com>
2021-11-19 10:20:52 +1300

    Added toast messages to posts

--- app/javascript/components/Post.jsx
+++ app/javascript/components/Post.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+/**
+ * @param {post} the post object, containing just a tweet for now. 
+ * @returns 
+ */
 const Post = ({post}) => {
 
   return (
--- app/javascript/components/Posts.jsx
+++ app/javascript/components/Posts.jsx
@@ -1,62 +1,89 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 
 import Post from "./Post";
 
 const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  const [post, setPost] = useState("");
-  const [reload, setReload] = useState(false);
-  
+  const [allPosts, setAllPosts] = useState([]);
+  const [currentPost, setCurrentPost] = useState("");
+  const [reload, setReload] = useState(false); // A hook for activating the useEffect hook to update the index of posts
+  const [toastMessage, setToastMessage] = useState(""); // The message of the toasts that this page displays
+
+  // Submits the current post inside the shown text area to the index
+  //  - Doesn't post if the post has no characters in it or too many characters..\
+  //  - Submits the post to the db index if text is valid.
   const submitPost = () => {
-      // Todo: add a toast or alert for tweet being too short or long!
-      if (post.length === 0 || post.length > 500) {
-          return;
-      }
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    const toast = new bootstrap.Toast(document.getElementById('postToast'));
 
-      const url = "/api/v1/posts/create";
-      const token = document.querySelector('meta[name="csrf-token"]').content;
-      fetch(url, { method: 'POST', headers: { "X-CSRF-Token": token, 'Content-Type': 'application/json'}, body: JSON.stringify({tweet: post})})
+    if (currentPost.length === 0) {
+      setToastMessage("Nothing to tweet!")
+      toast.show()
+      return
+    }
 
-      document.getElementById('post').value = "";
-      setPost("");
-      setReload(!reload);
+    if (currentPost.length > 500) {
+      setToastMessage("Tweet must be under 500 characters!")
+      toast.show()
+      return
+    }
+
+    const url = "/api/v1/posts/create";
+    fetch(url, { method: 'POST', headers: { "X-CSRF-Token": token, 'Content-Type': 'application/json' }, body: JSON.stringify({ tweet: currentPost }) })
+
+    document.getElementById('post').value = "";
+    setCurrentPost("");
+
+    setToastMessage("Tweeted!")
+    toast.show()
+    
+    setReload(!reload) // Trigger useEffect to update all posts
   }
 
   // Fetchs the posts
   useEffect(() => {
     const url = "/api/v1/posts/index.json";
     fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Network response was not ok.");
-      }})
       .then(response => {
-        setPosts(response);
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Network response was not ok.");
+        }
       })
-    }, [reload]);
+      .then(response => {
+        setAllPosts(response);
+      })
+  }, [reload]);
 
-    return (
-      <div className="container">
-        <div className="row">
-          <h1 className="display-1">Posts</h1>
-        </div>
-        <div className="row">
-            <div>
-                <textarea id="post" name="post" rows="6" required onChange={(e) => setPost(e.target.value)}/>
-            </div>
-            <div>
-                <button className="btn btn-primary" onClick={submitPost} >post</button>
-            </div>
-        </div>
-        <div className="row list-group">
-          <ul>{posts.map((post) => <li className="list-group-item" key={post.id}><Post post={post}/></li>)}</ul>
-        </div>
-        <div className="row">
+  return (
+    <div className="container">
+      {/* Heading */}
+      <div className="row">
+        <h1 className="display-1">Posts</h1>
+      </div>
+      {/* Create post */}
+      <div className="row">
+        <div className="col d-grid">
+          <div className="position-relative">
+            <textarea className="form-control" placeholder="What's happening?" id="post" style={{ resize: "none", height: "140px" }} required onChange={(e) => setCurrentPost(e.target.value)} />
+            <span className={`position-absolute badge rounded-pill ${(500 - currentPost.length < 0) ? "bg-danger" : "bg-secondary"}`} style={{ bottom: "8px", right: "16px" }}>{500 - currentPost.length}</span>
+          </div>
+          <button className="btn btn-primary" onClick={submitPost} >Tweet</button>
         </div>
       </div>
-    );
-  };
+      {/* List of all posts */}
+      <div className="row list-group">
+        <ul>{allPosts.map((post) => <li className="list-group-item" key={post.id}><Post post={post} /></li>)}</ul>
+      </div>
+      <div className="position-fixed top-0 end-0 p-3">
+        <div id="postToast" className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+          <div className="toast-body">
+            {toastMessage}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Posts;
